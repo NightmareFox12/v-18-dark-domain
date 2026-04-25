7a5549204a85f983236322ebf85d3f611aa6f588
nightmarefox12 <carloshenriquez392004@gmail.com>
2026-04-25 18:10:39 -0400

    feat: implement DialogueData scriptable object and initialize introductory game scene

---
+++ Assets/Data/Dialogues/DialogueData.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+// Este es el molde genérico para CUALQUIER diálogo
+[System.Serializable] 
+public class Dialogue 
+{
+    public string characterName; // Quién habla
+    [TextArea(3, 10)]
+    public string sentence;      // Qué dice
+}
+
+// Este es el creador de archivos
+[CreateAssetMenu(fileName = "DialogueData", menuName = "Narrative/Dialogue Data")]
+public class DialogueData : ScriptableObject
+{
+    public Dialogue[] dialogues; 
+}
